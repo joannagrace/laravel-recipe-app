f7d0291b22a020c822d4a8863c52765045964fd7
Joanna <joannagracecavosora@gmail.com>
2018-07-02 14:16:08 -0700

    initialize laravel recipe app

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,23 @@
+
+<nav class="navbar navbar-expand-sm bg-green navbar-dark sticky-top">
+    <div class="container">
+        <a class="navbar-brand" href=""><img src="{{url('images/logo.png')}}" alt="logo"></a>
+        <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Recipes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+        </li>
+        </ul>
+    </div>
+</nav>
